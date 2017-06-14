cc7c8ad363a82ac55bb5ed0524da3c30f153d6d8
Piasy <xz4215@gmail.com>
2017-06-14 22:13:52 +0800

    try to build Android image

---
+++ android/run.sh
@@ -0,0 +1,12 @@
+if [ "$ENABLE_SHADOW_SOCKS" == "true" ]
+then
+  sed -i 's/server-ip/"$SHADOW_SOCKS_SERVER_ADDR"/g' /etc/shadowsocks.json
+  sed -i 's/server-port/"$SHADOW_SOCKS_SERVER_PORT"/g' /etc/shadowsocks.json
+  sed -i 's/enc-method/"$SHADOW_SOCKS_ENC_METHOD"/g' /etc/shadowsocks.json
+  sed -i 's/your-password/"$SHADOW_SOCKS_ENC_PASS"/g' /etc/shadowsocks.json
+
+  sslocal -c /etc/shadowsocks.json -d start
+  proxychains bash
+fi
+
+source /build.sh
