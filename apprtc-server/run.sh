e5f5301c46c62e863f23bed7d8c9cca81c0e4588
Piasy <xz4215@gmail.com>
2018-12-26 22:01:46 +0800

    support multiple peers

--- apprtc-server/run.sh
+++ apprtc-server/run.sh
@@ -3,5 +3,7 @@ sed -i 's/wss:\/\//ws:\/\//g' /apprtc/out/app_engine/apprtc.py
 sed -i 's/https:\/\//http:\/\//g' /apprtc/out/app_engine/apprtc.py
 
 sed -i "s/SERVER_PUBLIC_IP/${PUBLIC_IP}/g" /apprtc/out/app_engine/constants.py
+sed -i "s/ROOM_SIZE_VALUE/${ROOM_SIZE_VALUE}/g" /apprtc/out/app_engine/constants.py
+sed -i "s/AUTO_DESTROY_SIZE_VALUE/${AUTO_DESTROY_SIZE_VALUE}/g" /apprtc/out/app_engine/constants.py
 
 supervisord -c /apprtc_supervisord.conf
