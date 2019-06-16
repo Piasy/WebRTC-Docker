9efecc559f748f7d74996ff57a14a7d15cdf488f
Piasy <xz4215@gmail.com>
2019-06-16 09:26:51 +0800

    use supervisor

--- apprtc-server/run.sh
+++ apprtc-server/run.sh
@@ -4,9 +4,4 @@ sed -i 's/https:\/\//http:\/\//g' /apprtc/out/app_engine/apprtc.py
 
 sed -i "s/SERVER_PUBLIC_IP/${PUBLIC_IP}/g" /apprtc/out/app_engine/constants.py
 
-nodejs ice.js 2>> /iceconfig.log &
-
-$GOPATH/bin/collidermain -port=8089 -tls=false --room-server=0.0.0.0 2>> /collider.log &
-dev_appserver.py /apprtc/out/app_engine --skip_sdk_update_check --enable_host_checking=False --host=0.0.0.0 2>> /room_server.log &
-
-turnserver -v -L 0.0.0.0 -a -f -r apprtc -c /etc/turnserver.conf --no-tls --no-dtls
+supervisord -c /apprtc_supervisord.conf
