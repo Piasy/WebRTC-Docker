7e2fe1b2779ac6d271bdfdda4452d70acdc84b7c
Piasy <xz4215@gmail.com>
2019-07-03 23:06:04 +0800

    use new multiple peer server

--- apprtc-server/run.sh
+++ apprtc-server/run.sh
@@ -1,4 +1,3 @@
-sed -i "s/SERVER_PUBLIC_IP/${PUBLIC_IP}/g" /ice.js
 sed -i 's/wss:\/\//ws:\/\//g' /apprtc/out/app_engine/apprtc.py
 sed -i 's/https:\/\//http:\/\//g' /apprtc/out/app_engine/apprtc.py
 
