90c96e209605ec6128f67ca0294b93fbb7c21998
Piasy <xz4215@gmail.com>
2019-07-06 19:44:42 +0800

    update: add user timeout check

--- apprtc-server/run.sh
+++ apprtc-server/run.sh
@@ -4,5 +4,6 @@ sed -i 's/https:\/\//http:\/\//g' /apprtc/out/app_engine/apprtc.py
 sed -i "s/SERVER_PUBLIC_IP/${PUBLIC_IP}/g" /apprtc/out/app_engine/constants.py
 sed -i "s/ROOM_SIZE_VALUE/${ROOM_SIZE_VALUE}/g" /apprtc/out/app_engine/constants.py
 sed -i "s/AUTO_DESTROY_SIZE_VALUE/${AUTO_DESTROY_SIZE_VALUE}/g" /apprtc/out/app_engine/constants.py
+sed -i "s/USER_TIMEOUT_SECONDS_VALUE/${USER_TIMEOUT_SECONDS_VALUE}/g" /apprtc/out/app_engine/constants.py
 
 supervisord -c /apprtc_supervisord.conf
